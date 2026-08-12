b99cf2e9f77d6e14a7c7cf146aa1a56f78a3072b
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2026-08-12 09:52:28 +0100

    Version v0.6.3 torch fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.localbabymonitor.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.6.2"
+        versionCode = 11
+        versionName = "0.6.3"
     }
 
     buildTypes {
