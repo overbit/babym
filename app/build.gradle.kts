840380395d3ffa6c48a11f6535c1c6b4c5e410d5
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2026-08-12 10:56:47 +0100

    Version v0.6.5 without remote torch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.localbabymonitor.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.6.4"
+        versionCode = 13
+        versionName = "0.6.5"
     }
 
     buildTypes {
