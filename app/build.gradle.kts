7879f3e0859701359ecd0bfce29cf7d62636c26c
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2026-08-13 13:12:52 +0100

    Prepare v0.6.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.localbabymonitor.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.6.5"
+        versionCode = 14
+        versionName = "0.6.6"
     }
 
     buildTypes {
